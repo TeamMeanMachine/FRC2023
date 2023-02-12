8afce377328153b71f02a30ca1305c0f257870b1
a-crowell <ayla.public@gmail.com>
2023-02-12 13:55:44 -0800

    Added hopefully working apriltag object

---
+++ src/main/kotlin/org/team2471/frc2023/AprilTag.kt
@@ -0,0 +1,108 @@
+package org.team2471.frc2023
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout
+import edu.wpi.first.apriltag.AprilTagFields
+import edu.wpi.first.math.Pair
+import edu.wpi.first.math.geometry.*
+import edu.wpi.first.networktables.NetworkTableInstance
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.photonvision.EstimatedRobotPose
+import org.photonvision.PhotonCamera
+import org.photonvision.PhotonPoseEstimator
+import org.photonvision.targeting.PhotonPipelineResult
+import org.team2471.frc.lib.coroutines.periodic
+import org.team2471.frc.lib.framework.use
+import org.team2471.frc.lib.units.asMeters
+import org.team2471.frc.lib.units.asRadians
+import org.team2471.frc.lib.units.degrees
+import org.team2471.frc.lib.units.inches
+import java.util.*
+import kotlin.math.abs
+
+object AprilTag {
+    private val pvTable = NetworkTableInstance.getDefault().getTable("photonvision")
+
+    private val tagPoseEntry = pvTable.getEntry("tagPose")
+    private val pvXEntry = pvTable.getEntry("xpos")
+    private val pvYEntry = pvTable.getEntry("ypos")
+    val aprilTagFieldLayout : AprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.kDefaultField.m_resourceFile)
+    val camFront = PhotonCamera("camFront")
+    val camBack = PhotonCamera("camBack")
+
+    const val maxAmbiguity = 0.1
+
+    var robotToCamFront: Transform3d = Transform3d(
+        Translation3d(6.5.inches.asMeters, -16.0.inches.asMeters, 2.0.inches.asMeters),
+        Rotation3d(0.0, 18.0.degrees.asRadians, 0.0)
+    )
+
+    var robotToCamBack = Transform3d(
+        Translation3d(6.5.inches.asMeters, -16.0.inches.asMeters, 2.0.inches.asMeters),
+        Rotation3d(0.0, 18.0.degrees.asRadians, 0.0)
+    )
+
+    //true means front cam
+    private fun useFrontCam(): Boolean {
+        return if (Drive.position.y > 0) {
+            Drive.position.angle < 90 || Drive.position.angle > 270
+        } else{
+            90 < Drive.position.angle && Drive.position.angle < 270
+        }
+    }
+
+    private fun customEstimatedPose(useFrontCam: Boolean): EstimatedRobotPose?{
+        val cameraResult = if (useFrontCam) {
+            camFront.latestResult
+        } else {
+            camBack.latestResult
+        }
+
+        if (!cameraResult.hasTargets()) {
+            return null
+        }
+        val bestResult = cameraResult.bestTarget
+        if (bestResult.poseAmbiguity >= maxAmbiguity) {
+            return null
+        }
+        val targetPosition: Optional<Pose3d> = aprilTagFieldLayout.getTagPose(bestResult.fiducialId)
+
+        if (targetPosition.isEmpty) {
+            return null
+        }
+
+        val robotToCam = if (useFrontCam) {
+            robotToCamFront.inverse()
+        } else {
+            robotToCamBack.inverse()
+        }
+
+        return EstimatedRobotPose(
+            targetPosition
+                .get()
+                .transformBy(bestResult.bestCameraToTarget.inverse())
+                .transformBy(robotToCam),
+            cameraResult.timestampSeconds
+        )
+        //val filterResults = cameraResult.getTargets().filter { it -> it.poseAmbiguity < maxAmbiguity }
+    }
+    init {
+        GlobalScope.launch {
+            periodic {
+                val maybePose = customEstimatedPose(useFrontCam())
+                if (maybePose != null) {
+                    val currentPose = maybePose.estimatedPose  //maybePose.get().estimatedPose
+                    val tagX = currentPose.x
+                    val tagY = currentPose.y
+                    val tagRot = currentPose.rotation
+                    // println("X: $tagX Y: $tagY")
+                    pvXEntry.setDouble(tagX)
+                    pvYEntry.setDouble(tagY)
+                    tagPoseEntry.setDoubleArray(doubleArrayOf(tagX, tagY, tagRot.angle))
+//                    Drive.position = Vector2(curepos.get().estimatedPose.x.meters.asFeet - (13 + 3.5/12), curepos.get().estimatedPose.y.meters.asFeet - (26 + 0.5/12))
+//                    println(Vector2((26 + 0.5/12) - curepos.get().estimatedPose.x.meters.asFeet , curepos.get().estimatedPose.y.meters.asFeet - (13 + 3.5/12)))
+                }
+            }
+        }
+    }
+}
