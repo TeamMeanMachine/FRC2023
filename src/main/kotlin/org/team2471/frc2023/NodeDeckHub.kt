5ca106891c3c81f96973c615a02904ff0439c50e
Justin Kilk <justin@kilk.com>
2023-03-05 17:00:46 -0800

    Added NodeDeck networkTable auto entry's isStartingInside got renamed and is now enum class StartingPoint

--- src/main/kotlin/org/team2471/frc2023/NodeDeckHub.kt
+++ src/main/kotlin/org/team2471/frc2023/NodeDeckHub.kt
@@ -9,25 +9,53 @@ import org.team2471.frc.lib.units.inches
 
 object NodeDeckHub {
     private val nodeDeckTable = NetworkTableInstance.getDefault().getTable("NodeDeck")
-    private val chargeInAutoEntry = nodeDeckTable.getEntry("ChargeInAuto")
-    private val isStartingInsideEntry = nodeDeckTable.getEntry("IsStartingInside")
+
     private val selectedNodeEntry = nodeDeckTable.getEntry("Selected Node")
+    private val isFloorConeEntry  = nodeDeckTable.getEntry("isFloorCone")
     private val shoulderCoastModeEntry = nodeDeckTable.getEntry("setShoulderCoastMode")
     private val shoulderBrakeModeEntry = nodeDeckTable.getEntry("setShoulderBrakeMode")
-    private val armPoseEntry = nodeDeckTable.getEntry("armPose")
-    private val isFloorConeEntry  = nodeDeckTable.getEntry("isFloorCone")
+
+    //auto entry's
+    private val startingPointEntry = nodeDeckTable.getEntry("Starting Point")
+    private val chargeInAutoEntry = nodeDeckTable.getEntry("ChargeInAuto")
+    private val amountOfAutoPiecesEntry = nodeDeckTable.getEntry("AmountOfAutoPieces")
+    private val autoOneEntry = nodeDeckTable.getEntry("1")
+    private val autoTwoEntry = nodeDeckTable.getEntry("2")
+    private val autoThreeEntry = nodeDeckTable.getEntry("3")
+    private val autoFourEntry = nodeDeckTable.getEntry("4")
+    private val autoFiveEntry = nodeDeckTable.getEntry("5")
+
+
+    private val armPoseEntry = nodeDeckTable.getEntry("armPose") //why is this in the NodeDeck table?
+
+
+    val selectedNode
+        get() = selectedNodeEntry.getInteger(0)
     val shoulderBrakeMode: Boolean
         get() = shoulderBrakeModeEntry.getBoolean(false)
     val shoulderCoastMode: Boolean
         get() = shoulderCoastModeEntry.getBoolean(false)
-    val chargeInAuto: Boolean
-        get() = chargeInAutoEntry.getBoolean(false)
-    val isStartingInside: Boolean
-        get() = isStartingInsideEntry.getBoolean(false)
-    val selectedNode
-        get() = selectedNodeEntry.getInteger(0)
     val isFloorCone: Boolean
         get() = isFloorConeEntry.getBoolean(false)
+
+    //auto variables
+    val chargeInAuto: Boolean
+        get() = chargeInAutoEntry.getBoolean(false)
+    val startingPoint: StartingPoint
+        get() = StartingPoint.valueOf(startingPointEntry.getString("INSIDE"))
+    val amountOfAutoPieces: Int
+        get() = amountOfAutoPiecesEntry.getInteger(0).toInt()
+    val firstAutoPiece: Int
+        get() = autoOneEntry.getInteger(0).toInt()
+    val secondAutoPiece: Int
+        get() = autoTwoEntry.getInteger(0).toInt()
+    val thirdAutoPiece: Int
+        get() = autoThreeEntry.getInteger(0).toInt()
+    val fourthAutoPiece: Int
+        get() = autoFourEntry.getInteger(0).toInt()
+    val fifthAutoPiece: Int //5 piece auto?
+        get() = autoFiveEntry.getInteger(0).toInt()
+
 
     private var lastNode: Long = 0
     private var lastFloorState = false
